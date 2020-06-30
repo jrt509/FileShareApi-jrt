5714f0eafea419f410ebdaacd865c0b04ae95b1b
Mac1 <bischi8@gmail.com>
2020-06-30 11:14:50 -0700

    added route

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import io
 
 
 app= Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = ""
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://jfdnxveubpnusw:616c5c49666d82238fdd1faf220768634d8fd5050307486a06864b95689b1ecc@ec2-3-216-129-140.compute-1.amazonaws.com:5432/des05bhaqs59th"
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
@@ -28,10 +28,27 @@ class File(db.Model):
 
 class FileSchema(ma.Schema):
     class Meta:
-        fields = ("id", "name", "file_type")
+        fields = ("id", "name", "file_type")    
 
 file_schema = FileSchema()
 files_schema = FileSchema(many=True)
+
+@app.route("/file/add", methods=["POST"])
+def add_file():
+    name = request.form.get("name")
+    file_type = request.form.get("type")
+    data = request.files.get("data")
+
+    new_file = File(name, file_type, data.read())
+    db.session.add(new_file)
+    db.session.commit()
+
+    return jsonify("File added successfully")
+
+@app.route("/file/get/data", methods=["GET"])
+def get_file_data():
+    file_data = db.session.query(File).all()
+    return jsonify(files_schema.dump(file_data))
 
 if __name__ == "__main__":
     app.run(debug=True)
